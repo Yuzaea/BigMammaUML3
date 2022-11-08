4b8d809ca6870c760f9056d58bb7064d844160db
Vnj22 <Vnj22@LAPTOP-BOP4E0VG>
2022-11-08 12:15:24 +0000

    Add project files.

---
+++ UML3/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigMammaUML3
+{
+    public class MenuItem : IMenuItem
+    {
+        public int Number => throw new NotImplementedException();
+
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public MenuType Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsVegan { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsOrganic { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public virtual string PrintInfo()
+        {
+            return null;
+        }
+    }
+
+}
